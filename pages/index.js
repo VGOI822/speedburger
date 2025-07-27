45f79f0c1fd390e0c361b02c0658d54ebbe83e6c
VGOI822 <teixeira@roe-gmbh.de>
2025-07-27 06:28:46 +0200

    endpoint added

--- pages/index.js
+++ pages/index.js
@@ -97,7 +97,7 @@ export default function Home({ opinioes }) {
 
 export async function getServerSideProps() {
   try {
-    const res = await fetch('https://www./api/avaliacoes');
+    const res = await fetch('https://www.speedburgercocal.com.br/api/avaliacoes');
     const opinioes = await res.json();
 
     return {
