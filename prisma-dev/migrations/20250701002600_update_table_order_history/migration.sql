bfe00f41818893bc6bf2d222acfc86f3c78268be
VGOI822 <teixeira@roe-gmbh.de>
2025-07-27 06:08:00 +0200

    set to production avaliacoes

--- prisma-dev/migrations/20250701002600_update_table_order_history/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `orderDateTime` on the `OrderHistory` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "OrderHistory" DROP COLUMN "orderDateTime";
